48e4f403b5f34f961a304f16d7f524f074b111be
mehul dubey <Devil9630>
2020-12-24 10:32:13 +0530

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,14 @@
 </head>
 <body>
     <a href="">About</a>
-<h1>Helow sage from laravel</h1>
+    <a href="">contact</a>
+    <a href="">Home</a>
+<h1>Hello Sage from Laravel, Home Page</h1>
+<form method="post" action="store-user">
+    @csrf
+    <input type="text" name="name" placeholder="Enter Your Name"/>
+    <input type="text" name="email" placeholder="Enter Your Email"/>
+    <input type="submit" />
+</form>
 </body>
 </html>
